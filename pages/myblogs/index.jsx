049fc0566d29ee35c590e2afd45559ca7a319bb7
anoj <anojadubey@gmail.com>
2023-05-13 19:37:41 +0530

    post changes p

--- pages/myblogs/index.jsx
+++ pages/myblogs/index.jsx
@@ -125,16 +125,13 @@ export const getServerSideProps = async (req, res) => {
   let user = cookie.username;
   if (!cookie) cookie = "";
   if (user) {
-    const response = await fetch(
-      `https://blog-app-nu-olive.vercel.app/api/myblog/`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username: user }),
-      }
-    );
+    const response = await fetch(`https://prodemic.vercel.app/api/myblog/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: user }),
+    });
     data = await response.json();
   }
   console.log(data);
